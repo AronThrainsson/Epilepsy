43b72eaeb034b8cc220aeca83184871924cb9fb1
AronThrainsson <siat@sundhedsdata.dk>
2025-04-11 11:03:41 +0200

    Update epilepsy.js

--- Frontend/app/epilepsy/epilepsy.js
+++ Frontend/app/epilepsy/epilepsy.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function EpilepsyHome() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome, Epilepsy User</Text>
+      <Text>Welcome, Epilepsy</Text>
     </View>
   );
 }
