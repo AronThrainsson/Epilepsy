565f5adc629cc7e4f5da66f1aa8dcb006ef9ffcb
AronThrainsson <siat@sundhedsdata.dk>
2025-04-14 14:58:39 +0200

    OBS på IP adresse når den skal køre på computeren

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 
-const LOCAL_IP = 'http://172.20.10.5:8080'; // Your Mac IP (for real device & simulator)
+// i terminal for at finde ip adresse: ipconfig getifaddr en0
+const LOCAL_IP = 'http://192.168.1.65:8080'; // Your Mac IP (for real device & simulator)
 
 export const BASE_URL =
   Platform.OS === 'android' && !__DEV__
